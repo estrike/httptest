1fed7ac87af7a9fe3e831aadd448b21bd464919c
Ethan Strike <eestrik@gmail.com>
2017-08-06 21:11:35 -0400

    Add command line arguments
    The following command line flags were added:
    -u : the URL to test, is mandatory
    -h : hostname for the Host header, if not set user http.Client value
    -s : ignore HTTP status code if non-200
    -n : number of passes

--- httptest.go
+++ httptest.go
@@ -1,48 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
-	"log"
-//	"flag"
 )
 
-var totalTime time.Duration = 0
-var average time.Duration
-var numPasses int = 10
+// Variables with command line flags
 var url string
+var numPasses int
+var ignoreStatus bool
+var hostname string
 
 func init() {
-	// Expects the URL on the commandline
-	numArgs := len(os.Args)
-	if numArgs >= 2 {
-		url = os.Args[1]
-	} else {
-		log.Fatal("URL is needed")
-	}
-
-	if numArgs == 3 {
-        	if result, err := strconv.Atoi(os.Args[2]); err == nil {
-			numPasses = result
-		}	
-	}
+	flag.StringVar(&hostname, "h", "", "Hostname for Host header")
+	flag.IntVar(&numPasses, "n", 10, "Number of connections")
+	flag.BoolVar(&ignoreStatus, "s", false, "Ignore returned HTTP status code")
+	flag.StringVar(&url, "u", "", "URL, including protocol (HTTP/HTTPS), mandatory")
 }
 
 func main() {
+	var totalTime time.Duration = 0
+
+	flag.Parse()
+
+	//Check if url is empty, if so exit
+	if url == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	// Prepare the HTTP client with URL and HEADERS
 	client := &http.Client{}
 	req, _ := http.NewRequest("HEAD", url, nil)
-	req.Host = "www.example.com"
 
-	// Prime DNS
+	if hostname != "" {
+		req.Host = hostname
+	}
+
+	// Prime DNS and verify connectivity
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
-	} else if resp.StatusCode != 200 {
-		log.Fatal ("Received a non-200 status code: ", resp.StatusCode)
+	} else if resp.StatusCode != 200 && !ignoreStatus {
+		log.Fatal("Received a non-200 status code: ", resp.StatusCode)
 	}
 
 	// Perform the passes, keeping track of how long each takes
@@ -50,16 +54,18 @@ func main() {
 		start := time.Now()
 		_, err := client.Do(req)
 		elapsed := time.Since(start)
+
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		fmt.Printf("Pass %d took %s\n", i, elapsed)
 		totalTime += elapsed
-			
+
 	}
 
-	average = totalTime / time.Duration(numPasses)
+	// Compute average and print stats
+	average := totalTime / time.Duration(numPasses)
 	fmt.Printf("Total time for %d passes = %v\n", numPasses, totalTime)
 	fmt.Printf("Average time for each pass = %v\n", average)
 }
